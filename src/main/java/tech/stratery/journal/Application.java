b6eb7eee6873f2a3888dbe8a6b88598b94f562c9
Унесихин Матвей <unesikhin.m@esphere.ru>
2021-02-07 19:09:31 +0500

    remove jsp add thymeleaf template

--- src/main/java/tech/stratery/journal/Application.java
+++ src/main/java/tech/stratery/journal/Application.java
@@ -1,14 +1,11 @@
 package tech.stratery.journal;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
-public class Application extends SpringBootServletInitializer {
+public class Application {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
